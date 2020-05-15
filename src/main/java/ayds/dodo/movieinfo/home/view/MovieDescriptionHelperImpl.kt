8229d3e847916099fc67b41273f07970800baca2
GeroLeLan <gerolelant@gmail.com>
2020-05-15 13:50:36 -0300

    pequeños cambios en MovieDescriptionHelperImpl
    cambio los "returns"  y creo la cuncion "getRatings" para que sea mas legible igualmente dejo el codigo original comentado por las dudas

--- src/main/java/ayds/dodo/movieinfo/home/view/MovieDescriptionHelperImpl.kt
+++ src/main/java/ayds/dodo/movieinfo/home/view/MovieDescriptionHelperImpl.kt
@@ -7,32 +7,50 @@ interface MovieDescriptionHelper {
 }
 
 internal class MovieDescriptionHelperImpl : MovieDescriptionHelper {
+
+    fun getRatings(movie: OmdbMovie): StringBuilder {
+        val ratings = StringBuilder()
+        for (rating in movie.ratings) {
+            when (rating.source) {
+                "Internet Movie Database" -> {
+                    val score = rating.value.split("/").toTypedArray()
+                    ratings.append("IMDB").append(" ").append(score[0]).append("\n")
+                }
+                "Rotten Tomatoes" -> ratings.append(rating.source).append(" ").append(rating.value).append("\n")
+                "Metacritic" -> {
+                    val score = rating.value.split("/").toTypedArray()
+                    ratings.append(rating.source).append(" ").append(score[0]).append("%").append("\n")
+                }
+                else -> ratings.append(rating.source).append(" ").append(rating.value).append("\n")
+            }
+        }
+        return ratings
+    }
     override fun getMovieDescriptionText(movie: OmdbMovie): String {
-        return if (movie.title.isEmpty()) {
-            "Movie not found"
+        if (movie.title.isEmpty()) {
+            return "Movie not found"
         } else {
-            val ratings = StringBuilder()
+            val ratings =getRatings(movie)
+            /*val ratings = StringBuilder()
             for (rating in movie.ratings) {
                 when (rating.source) {
                     "Internet Movie Database" -> {
                         val score = rating.value.split("/").toTypedArray()
                         ratings.append("IMDB").append(" ").append(score[0]).append("\n")
                     }
-                    "Rotten Tomatoes" -> ratings.append(rating.source).append(" ").append(rating.value).append(
-                        "\n"
-                    )
+                    "Rotten Tomatoes" -> ratings.append(rating.source).append(" ").append(rating.value).append("\n")
                     "Metacritic" -> {
                         val score = rating.value.split("/").toTypedArray()
                         ratings.append(rating.source).append(" ").append(score[0]).append("%").append("\n")
                     }
                     else -> ratings.append(rating.source).append(" ").append(rating.value).append("\n")
                 }
-            }
+            }*/
             var title = movie.title
             if (movie.isLocallyStoraged) {
                 title = "[*]" + movie.title
             }
-            ("<html><body style='width: 400px;'>" +
+            return ("<html><body style='width: 400px;'>" +
                     title + " - " + movie.year + "<br><br>"
                     + "Director: " + movie.director + "<br><br>"
                     + "Actors: " + movie.actors + "<br><br>"
