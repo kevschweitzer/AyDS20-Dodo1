82670d236f16137cdb47ed9e55a1739ce6cf73d7
GuidoChia <chiaguido@gmail.com>
2020-05-16 17:14:49 -0300

    Creo clase Movie Rating para respetar el principio de responsabilidad única

--- src/main/java/ayds/dodo/movieinfo/home/view/MovieDescriptionHelperImpl.kt
+++ src/main/java/ayds/dodo/movieinfo/home/view/MovieDescriptionHelperImpl.kt
@@ -7,45 +7,12 @@ interface MovieDescriptionHelper {
 }
 
 internal class MovieDescriptionHelperImpl : MovieDescriptionHelper {
-
-    fun getRatings(movie: OmdbMovie): StringBuilder {
-        val ratings = StringBuilder()
-        for (rating in movie.ratings) {
-            when (rating.source) {
-                "Internet Movie Database" -> {
-                    val score = rating.value.split("/").toTypedArray()
-                    ratings.append("IMDB").append(" ").append(score[0]).append("\n")
-                }
-                "Rotten Tomatoes" -> ratings.append(rating.source).append(" ").append(rating.value).append("\n")
-                "Metacritic" -> {
-                    val score = rating.value.split("/").toTypedArray()
-                    ratings.append(rating.source).append(" ").append(score[0]).append("%").append("\n")
-                }
-                else -> ratings.append(rating.source).append(" ").append(rating.value).append("\n")
-            }
-        }
-        return ratings
-    }
     override fun getMovieDescriptionText(movie: OmdbMovie): String {
         if (movie.title.isEmpty()) {
             return "Movie not found"
         } else {
-            val ratings =getRatings(movie)
-            /*val ratings = StringBuilder()
-            for (rating in movie.ratings) {
-                when (rating.source) {
-                    "Internet Movie Database" -> {
-                        val score = rating.value.split("/").toTypedArray()
-                        ratings.append("IMDB").append(" ").append(score[0]).append("\n")
-                    }
-                    "Rotten Tomatoes" -> ratings.append(rating.source).append(" ").append(rating.value).append("\n")
-                    "Metacritic" -> {
-                        val score = rating.value.split("/").toTypedArray()
-                        ratings.append(rating.source).append(" ").append(score[0]).append("%").append("\n")
-                    }
-                    else -> ratings.append(rating.source).append(" ").append(rating.value).append("\n")
-                }
-            }*/
+            val ratingHelper = MovieRatingHelperImpl()
+            val ratings = ratingHelper.getRatings(movie)
             var title = movie.title
             if (movie.isLocallyStoraged) {
                 title = "[*]" + movie.title
---
+++ src/main/java/ayds/dodo/movieinfo/home/view/MovieRatingHelperImpl.kt
@@ -0,0 +1,24 @@
+package ayds.dodo.movieinfo.home.view
+
+import ayds.dodo.movieinfo.home.model.entities.OmdbMovie
+
+class MovieRatingHelperImpl {
+    fun getRatings(movie: OmdbMovie): StringBuilder {
+        val ratings = StringBuilder()
+        for (rating in movie.ratings) {
+            when (rating.source) {
+                "Internet Movie Database" -> {
+                    val score = rating.value.split("/").toTypedArray()
+                    ratings.append("IMDB").append(" ").append(score[0]).append("\n")
+                }
+                "Rotten Tomatoes" -> ratings.append(rating.source).append(" ").append(rating.value).append("\n")
+                "Metacritic" -> {
+                    val score = rating.value.split("/").toTypedArray()
+                    ratings.append(rating.source).append(" ").append(score[0]).append("%").append("\n")
+                }
+                else -> ratings.append(rating.source).append(" ").append(rating.value).append("\n")
+            }
+        }
+        return ratings
+    }
+}
