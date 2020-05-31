07fea8c6d4d8945e4eb706e95f1a3339a8c22fb3
Sergio Andrade <sergio.david2201@gmail.com>
2020-05-31 15:30:41 -0300

    Cambios finales en DataBase.kt y OtherInfoWindow.java

--- src/main/java/ayds/dodo/movieinfo/moredetails/fulllogic/DataBase.kt
+++ src/main/java/ayds/dodo/movieinfo/moredetails/fulllogic/DataBase.kt
@@ -1,7 +1,6 @@
 package ayds.dodo.movieinfo.moredetails.fulllogic
 
 import java.sql.DriverManager
-import java.sql.ResultSet
 import java.sql.SQLException
 
 object DataBase {
@@ -13,45 +12,57 @@ object DataBase {
     private const val IMAGE_URL_COLUMN = "image_url"
     private const val CREATE_TABLE_QUERY = "create table $TABLE_NAME ($ID_COLUMN INTEGER PRIMARY KEY AUTOINCREMENT, $TITLE_COLUMN string, $PLOT_COLUMN string, $IMAGE_URL_COLUMN string)"
 
-    private fun getSelectQuery(title: String): String {
-        return "select * from info WHERE title = '$title'"
-    }
+    private fun getSelectQuery(title: String) = "select * from info WHERE title = '$title'"
 
-    private fun getInsertQuery(title: String, plot: String, imageUrl: String): String {
-        return "insert into info values(null, '$title', '$plot', '$imageUrl')"
+    private fun getInsertQuery(title: String, plot: String, imageUrl: String) = "insert into info values(null, '$title', '$plot', '$imageUrl')"
+
+    private fun getColumnByTitle(title: String, column: String): String? {
+        var toRet : String? = null
+        DriverManager.getConnection(URL).use { connection ->
+            try {
+                val statement = connection.createStatement()
+                statement.queryTimeout = 30
+                val rs = statement.executeQuery(getSelectQuery(title))
+                toRet = if (!rs.isClosed) {
+                    rs.next()
+                    rs.getString(column)
+                } else {
+                    null
+                }
+            } catch (e: SQLException) {
+                System.err.println("getImageUrl error " + e.message)
+            }
+        }
+        return toRet
     }
 
     @JvmStatic
     fun createNewDatabase() {
-        val connection = DriverManager.getConnection(URL)
-        val statement = connection.createStatement()
-        try {
-            if (connection != null) {
-                if (connection.metaData?.getTables(null, null, TABLE_NAME, null)?.next() == false) {
-                    statement.queryTimeout = 30
-                    statement.executeUpdate(CREATE_TABLE_QUERY)
+        DriverManager.getConnection(URL).use { connection ->
+            try {
+                val statement = connection.createStatement()
+                if (connection != null) {
+                    if (connection.metaData?.getTables(null, null, TABLE_NAME, null)?.next() == false) {
+                        statement.queryTimeout = 30
+                        statement.executeUpdate(CREATE_TABLE_QUERY)
+                    }
                 }
+            } catch (e: SQLException) {
+                System.err.println("Fallo al crear DB: " + e.message)
             }
-        } catch (e: SQLException) {
-            System.err.println("Fallo al crear DB: " + e.message)
-        } finally {
-            statement.close()
-            connection.close()
         }
     }
 
     @JvmStatic
     fun saveMovieInfo(title: String, plot: String, imageUrl: String) {
-        val connection = DriverManager.getConnection(URL)
-        val statement = connection.createStatement()
-        try {
-            statement.queryTimeout = 30
-            statement.executeUpdate(getInsertQuery(title, plot, imageUrl))
-        } catch (e: SQLException) {
-            System.err.println("Error on saving movie info " + e.message)
-        } finally {
-            statement.close()
-            connection.close()
+        DriverManager.getConnection(URL).use { connection ->
+            try {
+                val statement = connection.createStatement()
+                statement.queryTimeout = 30
+                statement.executeUpdate(getInsertQuery(title, plot, imageUrl))
+            } catch (e: SQLException) {
+                System.err.println("Error on saving movie info " + e.message)
+            }
         }
     }
 
@@ -63,24 +74,5 @@ object DataBase {
     @JvmStatic
     fun getImageUrl(title: String): String? {
         return getColumnByTitle(title, IMAGE_URL_COLUMN)
-    }
-
-    private fun getColumnByTitle(title: String, column: String): String? {
-        val connection = DriverManager.getConnection(URL)
-        val statement = connection.createStatement()
-        try {
-            statement.queryTimeout = 30
-            val rs = statement.executeQuery(getSelectQuery(title))
-            return if (!rs.isClosed) {
-                rs.next()
-                rs.getString(column)
-            } else null
-        } catch (e: SQLException) {
-            System.err.println("getImageUrl error " + e.message)
-        } finally {
-            statement.close()
-            connection.close()
-        }
-        return null
     }
 }
--- src/main/java/ayds/dodo/movieinfo/moredetails/fulllogic/OtherInfoWindow.java
+++ src/main/java/ayds/dodo/movieinfo/moredetails/fulllogic/OtherInfoWindow.java
@@ -1,43 +1,56 @@
 package ayds.dodo.movieinfo.moredetails.fulllogic;
 
 import ayds.dodo.movieinfo.home.model.entities.OmdbMovie;
+
 import com.google.gson.Gson;
-import com.google.gson.JsonElement;
 import com.google.gson.JsonNull;
 import com.google.gson.JsonObject;
+import com.google.gson.JsonElement;
+
 import org.jetbrains.annotations.NotNull;
 import org.jetbrains.annotations.Nullable;
+
 import retrofit2.Response;
 import retrofit2.Retrofit;
 import retrofit2.converter.scalars.ScalarsConverterFactory;
 
-import javax.imageio.ImageIO;
-import javax.swing.*;
-import javax.swing.event.HyperlinkEvent;
 import java.awt.*;
+import javax.swing.*;
+import javax.imageio.ImageIO;
 import java.awt.image.BufferedImage;
-import java.io.IOException;
+import javax.swing.event.HyperlinkEvent;
+
 import java.net.URL;
 import java.util.Iterator;
+import java.io.IOException;
 
 public class OtherInfoWindow {
     private JPanel contentPane;
     private JTextPane textPane2;
     private JPanel imagePanel;
+    private final String TMDB_URL_BASE = "https://api.themoviedb.org/3/";
+    private static final String FRAME_TITLE = "Movie Info Dodo";
+    private static final String MORE_DETAILS_HEADER = "Data from The Movie Data Base";
+    private static final String TEXT_TYPE = "text/html";
+    private static final String HTML_OPEN = "<html>";
+    private static final String BODY_STYLE = "<body style=\"width: 400px;\">";
+    private static final String FONT_STYLE_OPEN = "<font face=\"arial\">";
+    private static final String FONT_CLOSE = "</font>";
+    private static final String BOLD_OPEN = "<b>";
+    private static final String BOLD_CLOSE = "</b>";
 
     public void getMoviePlot(OmdbMovie movie) {
         Retrofit retrofit = new Retrofit.Builder()
-                .baseUrl("https://api.themoviedb.org/3/")
+                .baseUrl(TMDB_URL_BASE)
                 .addConverterFactory(ScalarsConverterFactory.create())
                 .build();
 
         TheMovieDBAPI tmdbAPI = retrofit.create(TheMovieDBAPI.class);
 
-        textPane2.setContentType("text/html");
+        textPane2.setContentType(TEXT_TYPE);
 
         textPane2.addHyperlinkListener(e -> {
             if (HyperlinkEvent.EventType.ACTIVATED.equals(e.getEventType())) {
-                System.out.println(e.getURL());
                 Desktop desktop = Desktop.getDesktop();
                 try {
                     desktop.browse(e.getURL().toURI());
@@ -58,7 +71,7 @@ public class OtherInfoWindow {
         win.contentPane.setLayout(new BoxLayout(win.contentPane, BoxLayout.PAGE_AXIS));
 
         JLabel label = new JLabel();
-        label.setText("Data from The Movie Data Base");
+        label.setText(MORE_DETAILS_HEADER);
         win.contentPane.add(label);
 
         win.imagePanel = new JPanel();
@@ -67,12 +80,12 @@ public class OtherInfoWindow {
         JPanel descriptionPanel = new JPanel();
         win.textPane2 = new JTextPane();
         win.textPane2.setEditable(false);
-        win.textPane2.setContentType("text/html");
+        win.textPane2.setContentType(TEXT_TYPE);
         win.textPane2.setMaximumSize(new Dimension(600, 400));
         descriptionPanel.add(win.textPane2);
         win.contentPane.add(descriptionPanel);
 
-        JFrame frame = new JFrame("Movie Info Dodo");
+        JFrame frame = new JFrame(FRAME_TITLE);
         frame.setMinimumSize(new Dimension(600, 600));
         frame.setContentPane(win.contentPane);
         frame.pack();
@@ -84,17 +97,17 @@ public class OtherInfoWindow {
     }
 
     public static String textToHtml(String text, String term) {
-        StringBuilder builder = new StringBuilder("<html><body style=\"width: 400px;\">");
+        StringBuilder builder = new StringBuilder(HTML_OPEN + BODY_STYLE);
 
-        builder.append("<font face=\"arial\">");
+        builder.append(FONT_STYLE_OPEN);
 
         String textWithBold = text
                 .replace("'", "`")
-                .replaceAll("(?i)" + term, "<b>" + term.toUpperCase() + "</b>");
+                .replaceAll("(?i)" + term, BOLD_OPEN + term.toUpperCase() + BOLD_CLOSE);
 
         builder.append(textWithBold);
 
-        builder.append("</font>");
+        builder.append(FONT_CLOSE);
 
         return builder.toString();
     }
@@ -103,6 +116,14 @@ public class OtherInfoWindow {
     private class OtherWindowRunnable implements Runnable {
         private final OmdbMovie movie;
         private final TheMovieDBAPI tmdbAPI;
+        private final String IMAGE_NOT_FOUND = "https://farm5.staticflickr.com/4363/36346283311_1dec5bb2c2.jpg";
+        private final String IMAGE_URL_BASE = "https://image.tmdb.org/t/p/w400/";
+        private final String RESULTS_JSON = "results";
+        private final String OVERVIEW_JSON = "overview";
+        private final String POSTER_PATH_JSON = "poster_path";
+        private final String BACKDROP_PATH_JSON = "backdrop_path";
+        private final String NO_RESULTS_MESSAGES = "No Results";
+        private final String RELEASE_DATE = "release_date";
 
         public OtherWindowRunnable(OmdbMovie movie, TheMovieDBAPI tmdbAPI) {
             this.movie = movie;
@@ -115,23 +136,27 @@ public class OtherInfoWindow {
 
             String path = DataBase.getImageUrl(movie.getTitle());
 
-            if (text != null && path != null) {
-                //text = "[*]" + text;
-            } else {
+            if (text == null || path == null) {
                 try {
+                    path = IMAGE_NOT_FOUND;
                     JsonObject result = getInfoFromTmdb();
-                    String backdropPath = getBackdrop(result);
+                    String backdropPath = null;
+                    JsonElement posterPath = JsonNull.INSTANCE;
+                    JsonElement extract = JsonNull.INSTANCE;
 
-                    JsonElement posterPath = result.get("poster_path");
+                    if (result != null){
+                        backdropPath = getBackdrop(result);
+                        posterPath = result.get(POSTER_PATH_JSON);
+                        extract = result.get(OVERVIEW_JSON);
+                    }
 
-                    JsonElement extract = result.get("overview");
-                    if (extract == null || posterPath == JsonNull.INSTANCE) {
-                        text = "No Results";
+                    if (extract == JsonNull.INSTANCE || posterPath == JsonNull.INSTANCE) {
+                        text = NO_RESULTS_MESSAGES;
                     } else {
                         text = formatText(posterPath, extract);
 
-                        if (backdropPath != null)
-                            path = "https://image.tmdb.org/t/p/w400/" + backdropPath;
+                        if (backdropPath != null && !backdropPath.equals(""))
+                            path = IMAGE_URL_BASE + backdropPath;
 
                         DataBase.saveMovieInfo(movie.getTitle(), text, path);
                     }
@@ -150,18 +175,22 @@ public class OtherInfoWindow {
             textPane2.setText(text);
         }
 
+        private String createMoviePosterAnchor(JsonElement path){
+            return "<a href="+ IMAGE_URL_BASE + path.getAsString() + ">View Movie Poster</a>";
+        }
+
         @NotNull
         private String formatText(JsonElement posterPath, JsonElement extract) {
             String text;
             text = extract.getAsString().replace("\\n", "\n");
             text = textToHtml(text, movie.getTitle());
-            text += "\n" + "<a href=https://image.tmdb.org/t/p/w400/" + posterPath.getAsString() + ">View Movie Poster</a>";
+            text += "\n" + createMoviePosterAnchor(posterPath);
             return text;
         }
 
         @Nullable
         private String getBackdrop(JsonObject result) {
-            JsonElement backdropPathJson = result.get("backdrop_path");
+            JsonElement backdropPathJson = result.get(BACKDROP_PATH_JSON);
 
             String backdropPath = null;
 
@@ -173,13 +202,9 @@ public class OtherInfoWindow {
 
         private void setImageLabel(String path) throws IOException {
             JLabel label;
-            if (path != null) {
-                URL url = new URL(path);
-                BufferedImage image = ImageIO.read(url);
-                label = new JLabel(new ImageIcon(image));
-            } else {
-                label = new JLabel("Image not found");
-            }
+            URL url = new URL(path);
+            BufferedImage image = ImageIO.read(url);
+            label = new JLabel(new ImageIcon(image));
             imagePanel.add(label);
 
             contentPane.validate();
@@ -194,7 +219,7 @@ public class OtherInfoWindow {
 
             while (resultIterator.hasNext()) {
                 result = resultIterator.next().getAsJsonObject();
-                String year = result.get("release_date").getAsString().split("-")[0];
+                String year = result.get(RELEASE_DATE).getAsString().split("-")[0];
 
                 if (year.equals(movie.getYear()))
                     break;
@@ -204,13 +229,12 @@ public class OtherInfoWindow {
 
         @NotNull
         private Iterator<JsonElement> getJsonElementIterator() throws java.io.IOException {
-            Response<String> callResponse;
-            callResponse = tmdbAPI.getTerm(movie.getTitle()).execute();
+            Response<String> callResponse = tmdbAPI.getTerm(movie.getTitle()).execute();
 
             Gson gson = new Gson();
             JsonObject jobj = gson.fromJson(callResponse.body(), JsonObject.class);
 
-            return jobj.get("results").getAsJsonArray().iterator();
+            return jobj.get(RESULTS_JSON).getAsJsonArray().iterator();
         }
     }
 }
