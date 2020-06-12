8453f6e21444fe04470b489450223c8f3e3fbbdb
GuidoChia <chiaguido@gmail.com>
2020-06-12 18:17:08 -0300

    Agrego helper que pasa la TmdbMovie a HTML.

--- src/main/java/ayds/dodo/movieinfo/moredetails/controller/MoreDetailsControllerImpl.kt
+++ src/main/java/ayds/dodo/movieinfo/moredetails/controller/MoreDetailsControllerImpl.kt
@@ -8,6 +8,7 @@ internal class MoreDetailsControllerImpl(
         // aca va el model y la view como private val
 ) : MoreDetailsController{
     /*
+    Esto no se si va porque more details no tiene eventos a los que responder.
     // Crear UIEvente de la more details view 
     private val observer: Observer<UiEvent> = object : Observer<UiEvent> {
         override fun update(value: UiEvent) {
