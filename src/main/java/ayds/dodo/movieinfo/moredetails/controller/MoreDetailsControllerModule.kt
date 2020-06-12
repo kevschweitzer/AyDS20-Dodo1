6ce6d21233780e44ec0bd85ca520a6ec60fdf5ac
GuidoChia <chiaguido@gmail.com>
2020-06-12 17:23:36 -0300

    Añado paquete controller junto con el MoreDetailsController y su módulo.

---
+++ src/main/java/ayds/dodo/movieinfo/moredetails/controller/MoreDetailsControllerModule.kt
@@ -0,0 +1,11 @@
+package ayds.dodo.movieinfo.moredetails.controller
+
+import ayds.dodo.movieinfo.home.controller.HomeControllerImpl
+import ayds.dodo.movieinfo.home.model.HomeModelModule
+import ayds.dodo.movieinfo.home.view.HomeViewModule
+
+object MoreDetailsControllerModule {
+    fun init() {
+        // HomeControllerImpl(MoreDetailsViewModule.homeView, MoreDetailsModelModule.homeModel)
+    }
+}
