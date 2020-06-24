dc810e277313f503d90999203c4edbbcf4a6fa43
GeroLeLan <gerolelant@gmail.com>
2020-06-24 17:22:23 -0300

    saco la constante NO_RESULTS_MESSAGES

--- src/main/java/ayds/dodo/movieinfo/moredetails/model/entities/NonExistentTmdbMovie.kt
+++ src/main/java/ayds/dodo/movieinfo/moredetails/model/entities/NonExistentTmdbMovie.kt
@@ -1,8 +1,7 @@
 package ayds.dodo.movieinfo.moredetails.model.entities
 
 object NonExistentTmdbMovie : TmdbMovie() {
-    private const val NO_RESULTS_MESSAGES = "No Results"
     const val IMAGE_NOT_FOUND = "https://farm5.staticflickr.com/4363/36346283311_1dec5bb2c2.jpg"
-    override var plot = NO_RESULTS_MESSAGES
+    override var plot = "No Results"
     override var imageUrl = IMAGE_NOT_FOUND
 }
